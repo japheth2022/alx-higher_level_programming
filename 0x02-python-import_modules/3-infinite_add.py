86e58b4de16ad1e4958f0e5ee81b836b773a7dea
japheth2022 <japhethmuli.mj@.com>
2022-09-23 08:36:44 -0700

    3-infinite_add.py

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+    ans = 0
+    num = (len(sys.argv) - 1)
+    for index in range(1, num + 1):
+        ans += int(sys.argv[index])
+    print("{:d}".format(ans))
