18373ded4f4aefd7019766985494b35f6fba995f
japheth2022 <japhethmuli.mj@.com>
2022-09-23 07:26:15 -0700

    1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,7 +2,7 @@
 from 1-calculation.py import add, div, mul, sub
 a = 10
 b = 5
-if __name__ = "__main__":
+if __name__ == "__main__":
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
     print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
     print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
