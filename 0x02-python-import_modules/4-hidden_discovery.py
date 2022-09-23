0da90425bd8f98e72ea6475c1839877b0d9b8774
japheth2022 <japhethmuli.mj@.com>
2022-09-23 08:55:35 -0700

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    import sys
-    index = 0
-    listed = (dir(hidden_4))
-    listlen = len(listed)
-        for elements in listed:
-            if elements[0] == '_':
-                continue
-            else:
-                print(elements)
+    name = dir(hidden_4)
+    for names in name:
+        if names[:2] != "__":
+            print(names)
