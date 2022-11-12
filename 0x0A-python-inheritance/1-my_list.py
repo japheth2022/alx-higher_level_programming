fb340e2416dc0192f22a1095b3a3a8a8493123f0
japheth2022 <japhethmuli.mj@.com>
2022-11-12 09:07:10 -0800

    1-my_list.py

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+""" insert 1-my_list module"""
+
+
+class Mylist:
+
+    """represents Mylist"""
+
+def print_sorted(self):
+
+    """prints sorted list"""
+
+    print(sorted(self))
