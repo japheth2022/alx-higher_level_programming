59d3adaf1c1a960c150541116ace6793e33aafd9
japheth2022 <japhethmuli.mj@.com>
2022-11-17 09:48:27 -0800

    5-save_to_json_file.py

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+"""Defines a method to save an object in json representation"""
+
+import json
 
 
+def save_to_json_file(my_obj, filename):
+    """write object to a file using json representation"""
+    with open(filename, "w") as f:
+        json.dump(my_obj, filename)
