b10c128fafd90d9e4352ff2b3e123b1a51677f8a
japheth2022 <japhethmuli.mj@.com>
2022-11-17 08:26:07 -0800

    1-write_file.py

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a function that writes a  string to a UTF8 file"""
+
+
+def write_file(filename="", text=""):
+
+    """writes a string and returns the number of characters"""
+    with open(filename="", mode='w', encoding=UTF-8) as f:
+        f.write()
+        return(f.write())
